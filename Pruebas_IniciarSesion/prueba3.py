a81e39ee1f42240a82c97486b16d3d9ac9a94caf
Anem26 <105009953+Anem26@users.noreply.github.com>
2022-12-09 10:06:37 -0500

    Add files via upload
    Pruebas de iniciar sesion + informe

---
+++ Pruebas_IniciarSesion/prueba3.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome('C:/Selenium/chromedriver')
+driver.get("http://localhost/qatar/login.php")
+
+#obtener elementos
+email = driver.find_element("id", "email")
+contrasena = driver.find_element("id","pass")
+
+#enviar datos a los elementos
+correo = "prueba@gmail.com"
+email.send_keys(correo)
+contrasena.send_keys("contraseña1")
+
+
+#presionar enter para validar
+contrasena.send_keys(Keys.ENTER)
+
+#url para validar las pruebas
+url_error = "http://localhost/qatar/error.html"
+url_true = "http://localhost/qatar/paginap.php"
+
+#funcion
+def iniciaSesion_emailInvalido():
+    if(driver.current_url == url_true):
+        return True
+    else:
+        return False
+time.sleep(1)
