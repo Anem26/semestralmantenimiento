7396ef714ecbf1aa8be82485d5815b05e77b4b5b
Anem26 <105009953+Anem26@users.noreply.github.com>
2022-12-09 10:16:54 -0500

    Add files via upload
    pruebas de registro + informe

---
+++ Pruebas_Registro/prueba4.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome(executable_path=r"C:\dchrome\chromedriver.exe")
+driver.get("http://localhost/qatar/regis.html")
+
+#obtener elementos
+email = driver.find_element("id", "correo")
+usuario = driver.find_element("id","user")
+contrasena = driver.find_element("id","pass")
+
+#enviar datos a los elementos
+username = "derek"
+correo = "derek@gmail.com"
+email.send_keys(correo)
+
+usuario.send_keys(username)
+
+contrasena.send_keys("1234")
+
+
+#presionar enter para validar
+contrasena.send_keys(Keys.ENTER)
+
+#url para validar las pruebas
+url_error = "http://localhost/qatar/error.html"
+url_true = "http://localhost/qatar/login.php"
+
+#funcion
+def registroCorreoDuplicado():
+    if(driver.current_url == url_error):
+        return True
+    else:
+        return False
+
+time.sleep(2)
